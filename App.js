bf0a105c8141cce0b2c699f8b8ec58a608fe0776
Joonatan N <Joona-ni@outlook.com>
2024-02-12 10:19:41 +0200

    RandomBeerTest
    Gets random beer from random-data-api.com

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet,View,Text } from 'react-native';
+
+const URL = 'https://random-data-api.com/api/v2/beers'
 
 export default function App() {
+
+const [brand, setBrand] = useState('')
+const [name, setName] = useState('')
+const [style, setStyle] = useState('')
+
+useEffect(() => {
+  fetch(URL)
+  .then(response => response.json())
+  .then((json) => {
+    setBrand(json.brand)
+    setName(json.name)
+    setStyle(json.style)
+  }).catch((error) => {
+    console.log(error)
+  })
+}, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Random beer fact</Text>
+      <Text>{brand}</Text>
+      <Text>{name}</Text>
+      <Text>{style}</Text>
     </View>
   );
 }
@@ -13,8 +35,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'lightblue',
+    alignItems:'center',
     justifyContent: 'center',
-  },
+    },
+    fields:{
+      flexDirection: 'row',
+      justifyContent: 'center',
+      padding: 30,
+    },
+    heading: {
+      fontWeight: 'bold',
+      fontSize: 20,
+    }
 });
